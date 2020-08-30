57f0e05f309c1bc3b267d83d45d84732e8565f74
Abdullah06444 <abdullah44gur@gmail.com>
2020-08-30 19:40:49 +0300

    Added CSharp Code

--- CSharp0009_ASampleCodeWithLoops.cs
+++ CSharp0009_ASampleCodeWithLoops.cs
@@ -6,7 +6,6 @@ namespace CSharp0009_ASampleCodeWithLoops
     {
         static void Main(string[] args)
         {
-
             int number, counter = 0;
             double sum = 0;
 
@@ -14,24 +13,24 @@ namespace CSharp0009_ASampleCodeWithLoops
             {    // -1 olana kadar devam et
 
                 bool isPrime = true, canDivideby10 = false;
-                
+
                 int i;
                 if (number % 10 == 0)
-                    canDivideby10 = true;                    
+                    canDivideby10 = true;
                 else
                     if (number < 2)
-                        isPrime = false;
-                    else
-                        for (i = 2; i <= (float)number / 2; i++)
+                    isPrime = false;
+                else
+                    for (i = 2; i <= (float)number / 2; i++)
+                    {
+
+                        if (number % i == 0)
                         {
 
-                            if (number % i == 0)
-                            {
-
-                                isPrime = false;
-                                break;
-                            }
+                            isPrime = false;
+                            break;
                         }
+                    }
 
                 if (!canDivideby10 && !isPrime)
                     sum += Math.Pow(number, 2);
